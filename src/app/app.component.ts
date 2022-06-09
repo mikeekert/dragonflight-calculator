1fd4e3585128466b37e3ae54bb5585699ee0a2e5
mekert <mekert@nrtwebservices.com>
2022-06-09 10:01:03 -0500

    first draft

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ITalent } from './models/talent';
+import { WowClass } from './models/wow-class';
+import { ClassesService } from './services/classes.service';
+import { TalentsService } from './services/talents.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'dragon-calculator';
+  talent_count: number = 0;
+  classList: WowClass[] = [];
+  activeClass: WowClass | null = null;
+  classTalents: ITalent[][][] = [];
+
+  constructor(private talentSvc: TalentsService, private classSvc: ClassesService) {
+  }
+
+  ngOnInit(): void {
+    this.talentSvc.counterObservable.subscribe(numb => {
+      this.talent_count = numb;
+    });
+    this.classSvc.activeClassObservable.subscribe(i => {
+      this.activeClass = i;
+      this.classTalents = this.talentSvc.generateTalentList();
+      this.talentSvc.pointCounter = 0;
+    });
+    this.classList = this.classSvc.wowClassList;
+    this.activeClass = this.classSvc.activeClass;
+  }
+
+  selectClass(i: WowClass) {
+    this.classSvc.activeClass = i;
+  }
+
+  reset() {
+    this.talentSvc.pointCounter = 0;
+    this.talentSvc.talentsSelected = [];
+    console.log(this.talentSvc.talentsSelected);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NodeComponent } from './node/node.component';
+import { SvgConnectorComponent } from './node/svg-connector/svg-connector.component';
+import { ClassesService } from './services/classes.service';
+import { TalentsService } from './services/talents.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NodeComponent,
+    SvgConnectorComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [ClassesService, TalentsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
